f348681035497b74fc980ca313616a6748803a04
cmah <cmah@ucalgary.ca>
2016-03-21 23:39:25 -0600

    Init JSON files

---
+++ courses.js
@@ -0,0 +1,464 @@
+//
+// COURSES
+// "type": "category", "type": "courses",
+// ""values": ["cid","faculty","num","cnum","cname",
+//				"ctype","consent","credit","lecture","lab","did"]
+
+courses = [
+	{
+		"type": "category",
+		"values": ["cid","faculty","num","cnum","cname",
+					"ctype","consent","credit","lecture","lab","did"]
+	}
+	{
+		"type": "course",
+		"values": [1,"SENG",301,"SENG 301","Software Analysis and Design",
+					"senior","",3,3,1,1]
+	}
+	{
+		"type": "course",
+		"values": [2,"SENG",401,"SENG 401","Software Architecture",
+					"senior","",3,3,2,10]
+	}
+	{
+		"type": "course",
+		"values": [3,"SENG",403,"SENG 403","Software Development in Teams and Organizations",
+					"senior","",3,3,3,9]
+	}
+	{
+		"type": "course",
+		"values": [4,"SENG",437,"SENG 437","Software Testing",
+					"senior","",3,3,2,10]
+	}
+	{
+
+		"type": "course",
+		"values": [5,"SENG",471,"SENG 471","Software Requirements Engineering",
+					"senior","",3,3,2,9]
+	}
+	{
+		"type": "course",
+		"values": [6,"SENG",501,"SENG 501","Advanced Topics in Software Engineering",
+					"senior","Department of Electrical and Computer Engineering or the Department of Computer Science",3,3,2,10]
+	}
+	{
+		"type": "course",
+		"values": [7,"SENG",511,"SENG 511","Software Process and Project Management",
+					"senior","",3,3,2,10]
+	}
+	{
+		"type": "course",
+		"values": [8,"SENG",513,"SENG 513","Web-Based Systems",
+					"senior","",3,3,2,19]
+	}
+	{
+		"type": "course",
+		"values": [9,"SENG",515,"SENG 515","Agile Software Engineering",
+					"senior","",3,3,2,9]
+	}
+	{
+		"type": "course",
+		"values": [10,"SENG",521,"SENG 521","Software Reliability and Software Quality",
+					"senior","",3,3,2,11]
+	}
+	{
+		"type": "course",
+		"values": [11,"SENG",523,"SENG 523","Formal Methods",
+					"senior","",3,3,2,10]
+	}
+	{
+		"type": "course",
+		"values": [12,"SENG",533,"SENG 533","Software Performance Evaluation",
+					"senior","",3,3,2,10]
+	}
+	{
+		"type": "course",
+		"values": [13,"SENG",541,"SENG 541","Fundamentals of Software Evolution and Reuse",
+					"senior","",3,3,2,10]
+	}
+	{
+		"type": "course",
+		"values": [14,"CPSC",101,"CPSC 101","Introduction to Unix",
+					"not included in GPA","",0.75,6,0,0]
+	}
+	{
+		"type": "course",
+		"values": [15,"CPSC",102,"CPSC 102","Advanced Unix",
+					"not included in GPA","",1.5,12,0,0]
+	}
+	{
+		"type": "course",
+		"values": [16,"CPSC",105,"CPSC 105","Introduction to the Analysis of Algorithms",
+					"not included in GPA","",1.5,12,0,0]
+	}
+	{
+		"type": "course",
+		"values": [17,"CPSC",217,"CPSC 217","Introduction to Computer Science for Multidisciplinary Studies I",
+					"junior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [18,"CPSC",219,"CPSC 219","Introduction to Computer Science for Multidisciplinary Studies II",
+					"junior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [19,"CPSC",231,"CPSC 231","Introduction to Computer Science for Computer Science Majors I",
+					"junior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [20,"CPSC",233,"CPSC 233","Introduction to Computer Science for Computer Science Majors II",
+					"junior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [21,"CPSC",235,"CPSC 235","Advanced Introduction to Computer Science",
+					"junior","Department of Computer Science",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [22,"CPSC",301,"CPSC 301","Large-Scale Software Development",
+					"senior","Department of Computer Science",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [23,"CPSC",313,"CPSC 313","Introduction to Computability",
+					"senior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [24,"CPSC",319,"CPSC 319","Data Structures, Algorithms, and Their Applications",
+					"senior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [25,"CPSC",329,"CPSC 329","Explorations in Information Security and Privacy",
+					"senior","",3,3,2,18]
+	}
+	{
+		"type": "course",
+		"values": [26,"CPSC",331,"CPSC 331","Data Structures, Algorithms, and Their Analysis",
+					"senior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [27,"CPSC",335,"CPSC 335","Intermediate Information Structures",
+					"senior","",3,3,2,0]
+	}
+	{
+		"type": "course",
+		"values": [28,"CPSC",355,"CPSC 355","Computing Machinery I",
+					"senior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [29,"CPSC",359,"CPSC 359","Computing Machinery II",
+					"senior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [30,"CPSC",401,"CPSC 401","Computer Structure",
+					"senior","",3,3,2,0]
+	}
+	{
+		"type": "course",
+		"values": [31,"CPSC",409,"CPSC 409","History of Computation",
+					"senior","",3,3,0,0]
+	}
+	{
+		"type": "course",
+		"values": [32,"CPSC",411,"CPSC 411","Compiler Construction I",
+					"senior","",3,3,2,12]
+	}
+	{
+		"type": "course",
+		"values": [33,"CPSC",413,"CPSC 413","Design and Analysis of Algorithms I",
+					"senior","Contact Department if taken CPSC 319 but not CPSC 105",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [34,"CPSC",418,"CPSC 418","Introduction to Cryptography",
+					"senior","Contact Department if taken CPSC 319 but not CPSC 105",3,3,2,15]
+	}
+	{
+		"type": "course",
+		"values": [35,"CPSC",433,"CPSC 433","Artificial Intelligence",
+					"senior","",3,3,2,6]
+	}
+	{
+		"type": "course",
+		"values": [36,"CPSC",441,"CPSC 441","Computer Networks",
+					"senior","",3,3,2,5]
+	}
+	{
+		"type": "course",
+		"values": [37,"CPSC",449,"CPSC 449","Programming Paradigms",
+					"senior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [38,"CPSC",541,"CPSC 453","Introduction to Computer Graphics",
+					"senior","",3,3,2,3]
+	}
+	{
+		"type": "course",
+		"values": [39,"CPSC",457,"CPSC 457","Principles of Operating Systems",
+					"senior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [40,"CPSC",461,"CPSC 461","Information Structures III",
+					"senior","",3,3,2,6]
+	}
+	{
+		"type": "course",
+		"values": [41,"CPSC",471,"CPSC 471","Data Base Management Systems",
+					"senior","",3,3,2,10]
+	}
+	{
+		"type": "course",
+		"values": [42,"CPSC",481,"CPSC 481","Human-Computer Interaction I",
+					"senior","",3,3,2,7]
+	}
+	{
+		"type": "course",
+		"values": [43,"CPSC",491,"CPSC 491","Techniques for Numerical Computation",
+					"senior","",3,3,2,16]
+	}
+	{
+		"type": "course",
+		"values": [44,"CPSC",501,"CPSC 501","Advanced Programming Techniques",
+					"senior","",3,3,2,0]
+	}
+	{
+		"type": "course",
+		"values": [45,"CPSC",502,"CPSC 502","Research Project",
+					"senior","Department of Computer Science, 3.30 GPA over the last 90 units",6,1,5,2]
+	}
+	{
+		"type": "course",
+		"values": [46,"CPSC",503,"CPSC 503","Project",
+					"senior","Department of Computer Science",3,1,5,2]
+	}
+	{
+		"type": "course",
+		"values": [47,"CPSC",510,"CPSC 510","Compiler Construction II",
+					"senior","",3,3,2,10]
+	}
+	{
+		"type": "course",
+		"values": [48,"CPSC",511,"CPSC 511","Introduction to Complexity Theory",
+					"senior","",3,3,1,16]
+	}
+	{
+		"type": "course",
+		"values": [49,"CPSC",513,"CPSC 513","Computability",
+					"senior","",3,3,2,16]
+	}
+	{
+		"type": "course",
+		"values": [50,"CPSC",517,"CPSC 517","Design and Analysis of Algorithms II",
+					"senior","",3,3,2,16]
+	}
+	{
+		"type": "course",
+		"values": [51,"CPSC",518,"CPSC 518","Introduction to Computer Algebra",
+					"senior","",3,3,2,16]
+	}
+	{
+		"type": "course",
+		"values": [52,"CPSC",519,"CPSC 519","Introduction to Quantum Computation",
+					"senior","",3,3,1,16]
+	}
+	{
+		"type": "course",
+		"values": [53,"CPSC",521,"CPSC 521","Foundations of Functional Programming",
+					"senior","",3,3,2,16]
+	}
+	{
+		"type": "course",
+		"values": [54,"CPSC",522,"CPSC 522","Introduction to Randomized Algorithms",
+					"senior","",3,3,2,16]
+	}
+	{
+		"type": "course",
+		"values": [55,"CPSC",525,"CPSC 525","Principles of Computer Security",
+					"senior","",3,3,2,18]
+	}
+	{
+		"type": "course",
+		"values": [56,"CPSC",526,"CPSC 526","Network Systems Security",
+					"senior","",3,3,2,18]
+	}
+	{
+		"type": "course",
+		"values": [57,"CPSC",527,"CPSC 527","Computer Viruses and Malware",
+					"senior","Department of Computer Science",3,3,0,11]
+	}
+	{
+		"type": "course",
+		"values": [58,"CPSC",528,"CPSC 528","Spam and Spyware",
+					"senior","Department of Computer Science",3,3,0,21]
+	}
+	{
+		"type": "course",
+		"values": [59,"CPSC",530,"CPSC 530","Information Theoretic Security",
+					"senior","",3,3,2,22]
+	}
+	{
+		"type": "course",
+		"values": [60,"CPSC",531,"CPSC 531","Systems Modelling and Simulation",
+					"senior","",3,3,2,6]
+	}
+	{
+		"type": "course",
+		"values": [61,"CPSC",535,"CPSC 535","Introduction to Image Analysis and Computer Vision",
+					"senior","",3,3,2,6]
+	}
+	{
+		"type": "course",
+		"values": [62,"CPSC",550,"CPSC 550","Systems Administration",
+					"senior","Department of Computer Science",6,2,3,23]
+	}
+	{
+		"type": "course",
+		"values": [63,"CPSC",559,"CPSC 559","Introduction to Distributed Systems",
+					"senior","",3,3,2,19]
+	}
+	{
+		"type": "course",
+		"values": [64,"CPSC",561,"CPSC 561","Introduction to Distributed Algorithms",
+					"senior","",3,3,2,20]
+	}
+	{
+		"type": "course",
+		"values": [65,"CPSC",565,"CPSC 565","Emergent Computing",
+					"senior","",3,3,1,0]
+	}
+	{
+		"type": "course",
+		"values": [66,"CPSC",567,"CPSC 567","Foundations of Multi-Agent Systems",
+					"senior","",3,3,1,19]
+	}
+	{
+		"type": "course",
+		"values": [67,"CPSC",568,"CPSC 568","Agent Communications",
+					"senior","Department of Computer Science",3,3,0,23]
+	}
+	{
+		"type": "course",
+		"values": [68,"CPSC",571,"CPSC 571","Design and Implementation of Database Systems",
+					"senior","",3,3,2,19]
+	}
+	{
+		"type": "course",
+		"values": [69,"CPSC",572,"CPSC 572","Fundamentals of Social Network Analysis and Data Mining",
+					"senior","",3,3,2,0]
+	}
+	{
+		"type": "course",
+		"values": [70,"CPSC",581,"CPSC 581","Human-Computer Interaction II",
+					"senior","Department of Computer Science",3,3,2,13]
+	}
+	{
+		"type": "course",
+		"values": [71,"CPSC",583,"CPSC 583","Introduction to Information Visualization",
+					"senior","",3,3,2,13]
+	}
+	{
+		"type": "course",
+		"values": [72,"CPSC",584,"CPSC 584","Human-Robot Interaction",
+					"senior","Department of Computer Science",3,3,2,13]
+	}
+	{
+		"type": "course",
+		"values": [73,"CPSC",585,"CPSC 585","Games Programming",
+					"senior","Department of Computer Science",3,3,2,4]
+	}
+	{
+		"type": "course",
+		"values": [74,"CPSC",587,"CPSC 587","Fundamentals of Computer Animation",
+					"senior","",3,3,2,8]
+	}
+	{
+		"type": "course",
+		"values": [75,"CPSC",589,"CPSC 589","Modelling for Computer Graphics",
+					"senior","",3,3,2,8]
+	}
+	{
+		"type": "course",
+		"values": [76,"CPSC",591,"CPSC 591","Rendering",
+					"senior","",3,3,2,8]
+	}
+	{
+		"type": "course",
+		"values": [77,"CPSC",594,"CPSC 594","Software Engineering Project",
+					"senior","Department of Computer Science",6,1,5,10]
+	}
+	{
+		"type": "course",
+		"values": [78,"CPSC",598,"CPSC 598","Special Topics in Computer Science",
+					"senior","Department of Computer Science",6,3,0,0]
+	}
+	{
+		"type": "course",
+		"values": [79,"CPSC",599,"CPSC 599","Special Topics in Computer Science",
+					"senior","Department of Computer Science",3,3,0,0]
+	}
+	{
+		"type": "course",
+		"values": [80,"PMAT",418,"PMAT 418","Introduction to Cryptography",
+					"senior","",3,3,0,17]
+	}
+	{
+		"type": "course",
+		"values": [81,"STAT",213,"STAT 213","Introduction to Statistics I",
+					"junior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [82,"STAT",321,"STAT 321","Introduction to Probability",
+					"junior","",3,3,1,24]
+	}
+	{
+		"type": "course",
+		"values": [83,"PHIL",279,"PHIL 279","Logic I",
+					"junior","",3,3,1,1]
+	}
+	{
+		"type": "course",
+		"values": [84,"PHIL",379,"PHIL 379","",
+					"senior","",3,3,0,25]
+	}
+	{
+		"type": "course",
+		"values": [85,"MATH",211,"MATH 211","Linear Methods I",
+					"junior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [86,"MATH",265,"MATH 265","University Calculus I",
+					"junior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [87,"MATH",271,"MATH 271","Discrete Mathematics",
+					"junior","",3,3,2,1]
+	}
+	{
+		"type": "course",
+		"values": [88,"MATH",267,"MATH 267","University Calculus II",
+					"junior","",3,3,2,8]
+	}
+	{
+		"type": "course",
+		"values": [89,"MATH",311,"MATH 311","Linear Methods II",
+					"junior","",3,3,1,16]
+	}
+	{
+		"type": "course",
+		"values": [90,"MATH",331,"MATH 331","Multivariate Calculus",
+					"junior","",3,3,1,6]
+	}
+]
---
+++ links.js
@@ -0,0 +1,804 @@
+// 
+// PREREQ
+// 
+// "type": "category", "type": "prereq",
+// "values": ["cidsource","isopt","cidtarget"]
+prereq = [
+	{
+		"type": "category",
+		"values": ["cidsource","isopt","cidtarget"]
+	}
+	{
+		"type": "prereq",
+		"values": [1,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [1,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [2,1,1]
+	}
+	{
+		"type": "prereq",
+		"values": [2,1,22]
+	}
+	{
+		"type": "prereq",
+		"values": [3,0,1]
+	}
+	{
+		"type": "prereq",
+		"values": [4,1,1]
+	}
+	{
+		"type": "prereq",
+		"values": [4,1,22]
+	}
+	{
+		"type": "prereq",
+		"values": [5,1,1]
+	}
+	{
+		"type": "prereq",
+		"values": [5,1,22]
+	}
+	{
+		"type": "prereq",
+		"values": [7,1,3]
+	}
+	{
+		"type": "prereq",
+		"values": [7,1,22]
+	}
+	{
+		"type": "prereq",
+		"values": [8,1,1]
+	}
+	{
+		"type": "prereq",
+		"values": [8,1,22]
+	}
+	{
+		"type": "prereq",
+		"values": [9,1,2]
+	}
+	{
+		"type": "prereq",
+		"values": [9,1,3]
+	}
+	{
+		"type": "prereq",
+		"values": [10,0,4]
+	}
+	{
+		"type": "prereq",
+		"values": [11,1,3]
+	}
+	{
+		"type": "prereq",
+		"values": [11,1,22]
+	}
+	{
+		"type": "prereq",
+		"values": [12,1,1]
+	}
+	{
+		"type": "prereq",
+		"values": [12,1,22]
+	}
+	{
+		"type": "prereq",
+		"values": [12,0,39]
+	}
+	{
+		"type": "prereq",
+		"values": [13,1,1]
+	}
+	{
+		"type": "prereq",
+		"values": [13,1,22]
+	}
+	{
+		"type": "prereq",
+		"values": [15,1,18]
+	}
+	{
+		"type": "prereq",
+		"values": [15,1,21]
+	}
+	{
+		"type": "prereq",
+		"values": [15,1,20]
+	}
+	{
+		"type": "prereq",
+		"values": [16,0,23]
+	}
+	{
+		"type": "prereq",
+		"values": [16,0,24]
+	}
+	{
+		"type": "prereq",
+		"values": [18,0,17]
+	}
+	{
+		"type": "prereq",
+		"values": [20,0,19]
+	}
+	{
+		"type": "prereq",
+		"values": [22,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [22,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [23,0,87]
+	}
+	{
+		"type": "prereq",
+		"values": [23,0,83]
+	}
+	{
+		"type": "prereq",
+		"values": [23,1,18]
+	}
+	{
+		"type": "prereq",
+		"values": [23,1,20]
+	}
+	{
+		"type": "prereq",
+		"values": [23,1,21]
+	}
+	{
+		"type": "prereq",
+		"values": [24,1,18]
+	}
+	{
+		"type": "prereq",
+		"values": [24,1,20]
+	}
+	{
+		"type": "prereq",
+		"values": [24,1,21]
+	}
+	{
+		"type": "prereq",
+		"values": [25,1,17]
+	}
+	{
+		"type": "prereq",
+		"values": [25,1,20]
+	}
+	{
+		"type": "prereq",
+		"values": [25,1,21]
+	}
+	{
+		"type": "prereq",
+		"values": [26,1,18]
+	}
+	{
+		"type": "prereq",
+		"values": [26,1,20]
+	}
+	{
+		"type": "prereq",
+		"values": [26,1,21]
+	}
+	{
+		"type": "prereq",
+		"values": [26,0,87]
+	}
+	{
+		"type": "prereq",
+		"values": [27,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [27,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [28,1,18]
+	}
+	{
+		"type": "prereq",
+		"values": [28,1,20]
+	}
+	{
+		"type": "prereq",
+		"values": [28,1,21]
+	}
+	{
+		"type": "prereq",
+		"values": [29,0,28]
+	}
+	{
+		"type": "prereq",
+		"values": [29,0,83]
+	}
+	{
+		"type": "prereq",
+		"values": [30,0,29]
+	}
+	{
+		"type": "prereq",
+		"values": [31,1,17]
+	}
+	{
+		"type": "prereq",
+		"values": [31,1,19]
+	}
+	{
+		"type": "prereq",
+		"values": [31,1,21]
+	}
+	{
+		"type": "prereq",
+		"values": [32,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [32,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [33,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [33,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [33,1,16]
+	}
+	{
+		"type": "prereq",
+		"values": [33,1,23]
+	}
+	{
+		"type": "prereq",
+		"values": [33,0,85]
+	}
+	{
+		"type": "prereq",
+		"values": [33,0,86]
+	}
+	{
+		"type": "prereq",
+		"values": [34,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [34,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [34,1,16]
+	}
+	{
+		"type": "prereq",
+		"values": [34,0,87]
+	}
+	{
+		"type": "prereq",
+		"values": [35,0,23]
+	}
+	{
+		"type": "prereq",
+		"values": [36,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [36,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [36,0,29]
+	}
+	{
+		"type": "prereq",
+		"values": [37,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [37,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [37,0,83]
+	}
+	{
+		"type": "prereq",
+		"values": [38,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [38,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [38,0,85]
+	}
+	{
+		"type": "prereq",
+		"values": [38,0,29]
+	}
+	{
+		"type": "prereq",
+		"values": [39,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [39,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [39,0,29]
+	}
+	{
+		"type": "prereq",
+		"values": [40,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [40,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [40,0,28]
+	}
+	{
+		"type": "prereq",
+		"values": [41,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [41,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [42,1,1]
+	}
+	{
+		"type": "prereq",
+		"values": [42,1,22]
+	}
+	{
+		"type": "prereq",
+		"values": [43,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [43,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [43,0,85]
+	}
+	{
+		"type": "prereq",
+		"values": [43,0,86]
+	}
+	{
+		"type": "prereq",
+		"values": [44,0,37]
+	}
+	{
+		"type": "prereq",
+		"values": [47,0,23]
+	}
+	{
+		"type": "prereq",
+		"values": [47,0,28]
+	}
+	{
+		"type": "prereq",
+		"values": [47,0,32]
+	}
+	{
+		"type": "prereq",
+		"values": [48,0,33]
+	}
+	{
+		"type": "prereq",
+		"values": [49,0,23]
+	}
+	{
+		"type": "prereq",
+		"values": [50,0,33]
+	}
+	{
+		"type": "prereq",
+		"values": [51,0,33]
+	}
+	{
+		"type": "prereq",
+		"values": [51,0,85]
+	}
+	{
+		"type": "prereq",
+		"values": [52,0,33]
+	}
+	{
+		"type": "prereq",
+		"values": [52,0,89]
+	}
+	{
+		"type": "prereq",
+		"values": [53,0,23]
+	}
+	{
+		"type": "prereq",
+		"values": [53,0,37]
+	}
+	{
+		"type": "prereq",
+		"values": [54,0,33]
+	}
+	{
+		"type": "prereq",
+		"values": [55,0,39]
+	}
+	{
+		"type": "prereq",
+		"values": [55,0,87]
+	}
+	{
+		"type": "prereq",
+		"values": [56,0,36]
+	}
+	{
+		"type": "prereq",
+		"values": [57,0,26]
+	}
+	{
+		"type": "prereq",
+		"values": [57,0,39]
+	}
+	{
+		"type": "prereq",
+		"values": [58,0,23]
+	}
+	{
+		"type": "prereq",
+		"values": [58,0,39]
+	}
+	{
+		"type": "prereq",
+		"values": [59,1,18]
+	}
+	{
+		"type": "prereq",
+		"values": [59,1,20]
+	}
+	{
+		"type": "prereq",
+		"values": [59,1,21]
+	}
+	{
+		"type": "prereq",
+		"values": [59,0,87]
+	}
+	{
+		"type": "prereq",
+		"values": [59,0,82]
+	}
+	{
+		"type": "prereq",
+		"values": [60,0,39]
+	}
+	{
+		"type": "prereq",
+		"values": [60,1,82]
+	}
+	{
+		"type": "prereq",
+		"values": [60,1,81]
+	}
+	{
+		"type": "prereq",
+		"values": [61,1,89]
+	}
+	{
+		"type": "prereq",
+		"values": [61,1,90]
+	}
+	{
+		"type": "prereq",
+		"values": [62,0,39]
+	}
+	{
+		"type": "prereq",
+		"values": [63,0,39]
+	}
+	{
+		"type": "prereq",
+		"values": [64,0,33]
+	}
+	{
+		"type": "prereq",
+		"values": [65,0,35]
+	}
+	{
+		"type": "prereq",
+		"values": [66,0,39]
+	}
+	{
+		"type": "prereq",
+		"values": [66,0,35]
+	}
+	{
+		"type": "prereq",
+		"values": [67,0,35]
+	}
+	{
+		"type": "prereq",
+		"values": [68,0,40]
+	}
+	{
+		"type": "prereq",
+		"values": [68,0,41]
+	}
+	{
+		"type": "prereq",
+		"values": [69,0,68]
+	}
+	{
+		"type": "prereq",
+		"values": [70,0,42]
+	}
+	{
+		"type": "prereq",
+		"values": [71,1,24]
+	}
+	{
+		"type": "prereq",
+		"values": [71,1,26]
+	}
+	{
+		"type": "prereq",
+		"values": [72,0,42]
+	}
+	{
+		"type": "prereq",
+		"values": [73,0,38]
+	}
+	{
+		"type": "prereq",
+		"values": [74,0,38]
+	}
+	{
+		"type": "prereq",
+		"values": [75,0,38]
+	}
+	{
+		"type": "prereq",
+		"values": [76,0,38]
+	}
+	{
+		"type": "prereq",
+		"values": [80,0,87]
+	}
+	{
+		"type": "prereq",
+		"values": [82,0,88]
+	}
+	{
+		"type": "prereq",
+		"values": [84,0,83]
+	}
+	{
+		"type": "prereq",
+		"values": [88,0,86]
+	}
+	{
+		"type": "prereq",
+		"values": [89,0,85]
+	}
+	{
+		"type": "prereq",
+		"values": [90,0,85]
+	}
+	{
+		"type": "prereq",
+		"values": [90,0,88]
+	}
+
+]
+
+// 
+// ANTIREQ
+// "type": "category", "type": "antireq",
+// "values": ["cidsource","isopt","cidtarget"]
+
+antireq = [
+	{
+		"type": "category",
+		"values": ["cidsource","isopt","cidtarget"]
+	}
+	{
+		"type": "antireq",
+		"values": [1,0,22]
+	}
+	{
+		"type": "antireq",
+		"values": [3,0,22]
+	}
+	{
+		"type": "antireq",
+		"values": [17,1,19]
+	}
+	{
+		"type": "antireq",
+		"values": [17,1,21]
+	}
+	{
+		"type": "antireq",
+		"values": [18,1,20]
+	}
+	{
+		"type": "antireq",
+		"values": [18,1,21]
+	}
+	{
+		"type": "antireq",
+		"values": [19,0,17]
+	}
+	{
+		"type": "antireq",
+		"values": [20,1,18]
+	}
+	{
+		"type": "antireq",
+		"values": [20,1,21]
+	}
+	{
+		"type": "antireq",
+		"values": [21,1,17]
+	}
+	{
+		"type": "antireq",
+		"values": [21,1,18]
+	}
+	{
+		"type": "antireq",
+		"values": [22,1,1]
+	}
+	{
+		"type": "antireq",
+		"values": [22,1,3]
+	}
+	{
+		"type": "antireq",
+		"values": [24,0,26]
+	}
+	{
+		"type": "antireq",
+		"values": [26,0,24]
+	}
+	{
+		"type": "antireq",
+		"values": [34,0,80]
+	}
+	{
+		"type": "antireq",
+		"values": [45,0,46]
+	}
+	{
+		"type": "antireq",
+		"values": [46,0,45]
+	}
+	{
+		"type": "antireq",
+		"values": [80,0,34]
+	}
+]
+
+// 
+// RECOMMENDED
+// "type": "category", "type": "recommended",
+// "values": ["cidsource","isopt","cidtarget"]
+
+recommended = [
+	{
+		"type": "category",
+		"values": ["cidsource","isopt","cidtarget"]
+	}
+	{
+		"type": "recommended",
+		"values": [17,0,14]
+	}
+	{
+		"type": "recommended",
+		"values": [19,0,14]
+	}
+	{
+		"type": "recommended",
+		"values": [23,1,24]
+	}
+	{
+		"type": "recommended",
+		"values": [23,1,26]
+	}
+	{
+		"type": "recommended",
+		"values": [25,1,85]
+	}
+	{
+		"type": "recommended",
+		"values": [25,1,86]
+	}
+	{
+		"type": "recommended",
+		"values": [25,1,87]
+	}
+	{
+		"type": "recommended",
+		"values": [32,0,23]
+	}
+	{
+		"type": "recommended",
+		"values": [33,0,86]
+	}
+	{
+		"type": "recommended",
+		"values": [35,0,37]
+	}
+	{
+		"type": "recommended",
+		"values": [51,0,43]
+	}
+	{
+		"type": "recommended",
+		"values": [54,0,82]
+	}
+	{
+		"type": "recommended",
+		"values": [55,0,25]
+	}
+	{
+		"type": "recommended",
+		"values": [56,0,25]
+	}
+	{
+		"type": "recommended",
+		"values": [56,1,34]
+	}
+	{
+		"type": "recommended",
+		"values": [56,1,80]
+	}
+	{
+		"type": "recommended",
+		"values": [59,0,25]
+	}
+	{
+		"type": "recommended",
+		"values": [60,0,82]
+	}
+	{
+		"type": "recommended",
+		"values": [60,0,36]
+	}
+	{
+		"type": "recommended",
+		"values": [71,1,38]
+	}
+	{
+		"type": "recommended",
+		"values": [71,1,42]
+	}
+	{
+		"type": "recommended",
+		"values": [87,0,83]
+	}
+]
